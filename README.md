e0f2cf3c643bf408ec96e1106fde13a17835beb2
Osani <jackahansen18@gmail.com>
2021-02-09 13:38:12 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Fitness Bot
+
+## What it does
+
+Randomly sends a message telling you to do fitness whenever you send something in discord
+
+## How to run
+
+`python3 fitness.py <insert discord token here>`
+
---
+++ fitness.py
@@ -0,0 +1,61 @@
+import discord
+from discord.ext import commands
+from datetime import datetime
+import sys
+import random
+
+client = commands.Bot(command_prefix="!")
+
+token = sys.argv[1]
+
+ODD = 50
+
+def get_exercise():
+
+    exerciseList = {
+        "push-ups" : random.randint(1,4)*5,
+        "sit-ups" : random.randint(2,5)*5,
+        "squats" : random.randint(3,6)*5,
+        "burpees" : random.randint(1,2)*5
+    }
+
+    return random.choice(list(exerciseList.items()))
+
+
+
+@client.event
+async def on_ready():
+    print(f"Bot started at {datetime.now()}")
+
+
+@client.event
+async def on_message(msg):
+
+    if msg.author == client.user:
+        return
+
+    num = random.randint(1,ODD)
+    
+    exercise = get_exercise()
+
+
+    if (num==1):
+        await msg.channel.send(msg.author.mention + f": you posted cringe, do {exercise[1]} {exercise[0]}")
+    
+
+    await client.process_commands(msg)
+
+@client.command(hidden=True)
+async def odds(ctx, newOdd):
+    if (not ctx.author.guild_permissions.administrator):
+        await ctx.send("You do not have the perms needed, you nerd")
+        return
+
+    global ODD
+
+    ODD = int(newOdd)
+    await ctx.send(f"New odds: 1 in {newOdd}")
+
+
+
+client.run(token)
